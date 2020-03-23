27fb3366624866f0a12a03b704f98dc07565f963
YanG-6668 <goch.yannn@gmail.com>
2020-03-23 13:24:55 +0200

    save

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,12 @@ trigger.addEventListener('click', () => {
         trigger.innerHTML = '<i class="icon-search"></i>';
     }
 });
+
+$('.ourCompany__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+    $('.ourCompany__tabs').find('.tab__item').removeClass('active-tab').hide();
+    $('.ourCompany__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+});
