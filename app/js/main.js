67c928a466929b61019a30b2b654fcb79d7677b0
YanG-6668 <goch.yannn@gmail.com>
2020-03-27 09:28:46 +0200

    added mobile header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 const trigger = document.querySelector('.trigger');
 const input = document.querySelector('.input');
 
+// const menuBtn = document.querySelector('.menu__btn');
+// const nav = document.querySelector('.menu__boss'); //full-scr
+// const menuList = document.querySelector('.header__top-menu-list');
+
+// menuBtn.addEventListener('click', function() {
+//     nav.classList.add('open-nav');
+// });
+// menuList.addEventListener('click', function() {
+//     nav.classList.remove('open-nav');
+// });
+
 trigger.addEventListener('click', () => {
     if (!input.classList.contains('input-open')) {
         input.classList.add('input-open');
@@ -10,6 +21,10 @@ trigger.addEventListener('click', () => {
     }
 });
 $(function() {
+
+    $('.menu__btn').on('click', function() {
+        $('.header__top-menu-list').slideToggle();
+    });
 
     $('.ourCompany__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
