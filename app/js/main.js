1b761c0c81918d99367979f1a4e8d754ed179ae7
YanG-6668 <goch.yannn@gmail.com>
2020-09-01 16:40:25 +0300

    fixed 1.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,6 @@
 const trigger = document.querySelector('.trigger');
 const input = document.querySelector('.input');
 
-// const menuBtn = document.querySelector('.menu__btn');
-// const nav = document.querySelector('.menu__boss'); //full-scr
-// const menuList = document.querySelector('.header__top-menu-list');
-
-// menuBtn.addEventListener('click', function() {
-//     nav.classList.add('open-nav');
-// });
-// menuList.addEventListener('click', function() {
-//     nav.classList.remove('open-nav');
-// });
-
 trigger.addEventListener('click', () => {
     if (!input.classList.contains('input-open')) {
         input.classList.add('input-open');
